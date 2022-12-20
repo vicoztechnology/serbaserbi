cc5dec8ad752f77a7fa7c9664e0f470ca97cf186
tofik <vicoztechnology@gmail.com>
2022-12-20 12:24:37 +0700

    update halaman home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,28 +1,34 @@
-
-
-  <Swiper
-  pagination={true} modules={[Pagination]}
-    spaceBetween={1}
-    slidesPerView={1}
-    on:slideChange={() => console.log('slide change')}
-    on:swiper={(e) => console.log(e.detail[0])}
-  >
-    <SwiperSlide class = "py-3 px-3 rounded-3xl"><img class="rounded-3xl" src="https://i.picsum.photos/id/26/500/250.jpg?hmac=aEMDV8A-c0ciLRML8vyh9VN_ur-69jZxJjlwNbyQOEc" alt="banner1"></SwiperSlide>
-    <SwiperSlide class = "py-3 px-3 rounded-3xl"><img class="rounded-3xl" src="https://i.picsum.photos/id/1066/500/250.jpg?hmac=pokx9pD_tKNT127IchfxrNPfCjxRtPHRK-m-b6dgrqM" alt="banner2"></SwiperSlide>
-    <SwiperSlide class = "py-3 px-3 rounded-3xl"><img class="rounded-3xl" src="https://i.picsum.photos/id/26/500/250.jpg?hmac=aEMDV8A-c0ciLRML8vyh9VN_ur-69jZxJjlwNbyQOEc" alt="banner1"></SwiperSlide>
-    <SwiperSlide class = "py-3 px-3 rounded-3xl"><img class="rounded-3xl" src="https://i.picsum.photos/id/1066/500/250.jpg?hmac=pokx9pD_tKNT127IchfxrNPfCjxRtPHRK-m-b6dgrqM" alt="banner2"></SwiperSlide>
-   
-    ...
-  </Swiper>
-  <script>
+<script>
   // Import Swiper Svelte components
   import { Swiper, SwiperSlide } from 'swiper/svelte';
   import "swiper/css/pagination"
-  import { Pagination } from "swiper";
+  import { Pagination, Autoplay } from "swiper";
 
   // Import Swiper styles
   import 'swiper/css';
   </script>
+
+  <Swiper
+  pagination={true}
+   modules={[Pagination, Autoplay]}
+    spaceBetween={1}
+    slidesPerView={1}
+    loop={true}
+    autoplay={{
+      delay:2500,
+      disableOnInteraction:false,
+    }}
+    on:slideChange={() => console.log('slide change')}
+    on:swiper={(e) => console.log(e.detail[0])}
+  >
+    <SwiperSlide class = "py-3 px-3 rounded-3xl"><img class="rounded-3xl" src="https://i.picsum.photos/id/26/500/250.jpg?hmac=aEMDV8A-c0ciLRML8vyh9VN_ur-69jZxJjlwNbyQOEc" alt="banner1" width="500px" height="250px"></SwiperSlide>
+    <SwiperSlide class = "py-3 px-3 rounded-3xl"><img class="rounded-3xl" src="https://i.picsum.photos/id/1066/500/250.jpg?hmac=pokx9pD_tKNT127IchfxrNPfCjxRtPHRK-m-b6dgrqM" alt="banner2" width="500px" height="250px"></SwiperSlide>
+    <SwiperSlide class = "py-3 px-3 rounded-3xl"><img class="rounded-3xl" src="https://i.picsum.photos/id/26/500/250.jpg?hmac=aEMDV8A-c0ciLRML8vyh9VN_ur-69jZxJjlwNbyQOEc" alt="banner3" width="500px" height="250px"></SwiperSlide>
+    <SwiperSlide class = "py-3 px-3 rounded-3xl"><img class="rounded-3xl" src="https://i.picsum.photos/id/1066/500/250.jpg?hmac=pokx9pD_tKNT127IchfxrNPfCjxRtPHRK-m-b6dgrqM" alt="banner4" width="500px" height="250px"></SwiperSlide>
+   
+    
+  </Swiper>
+
 <style>
  
 </style>
