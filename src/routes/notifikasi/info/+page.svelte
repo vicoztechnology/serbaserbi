8e90c1d1af5ce11253718a539812e3465c359f2e
tofik <vicoztechnology@gmail.com>
2023-01-27 15:23:05 +0700

    update

--- src/routes/notifikasi/info/+page.svelte
+++ src/routes/notifikasi/info/+page.svelte
@@ -1,1 +1,103 @@
-<div class="pt-10">Informasi</div>
+<script>
+	import PageTransition from '$lib/components/PageTransition.svelte';
+</script>
+
+<PageTransition>
+	<div
+		class="notifikasi text-on-background-light dark:text-on-background-dark transition-colors duration-1000"
+	>
+		<div class="promo pt-6 divide-y divide-outline-variant-light dark:divide-outline-variant-dark ">
+			<div class="flex">
+				<div class="flex-none my-auto pl-4">
+					<img
+						src="https://fastly.picsum.photos/id/1071/50/50.jpg?hmac=8UEBgUSeqRWF6cy_Hgn3imi8Sjt-AZFtGtnInAiMrKA"
+						alt="gambar promo"
+						loading="lazy"
+						class="inset-0 rounded-xl "
+					/>
+				</div>
+				<form class="flex-auto p-3">
+					<div class="flex flex-wrap">
+						<div class="flex flex-nowrap">
+							<svg
+								class="fill-tertiary-light dark:fill-tertiary-dark transition-colors duration-1000 "
+								xmlns="http://www.w3.org/2000/svg"
+								height="20"
+								width="20"
+								><path
+									d="M4.208 15.833q-.354 0-.614-.26-.261-.261-.261-.615t.261-.614q.26-.261.614-.261h.771V8.354q0-1.708 1.042-3.052 1.041-1.344 2.687-1.76v-.584q0-.541.375-.916T10 1.667q.542 0 .917.375t.375.916v.584q1.646.416 2.677 1.76Q15 6.646 15 8.354v5.729h.792q.354 0 .614.261.261.26.261.614t-.261.615q-.26.26-.614.26Zm5.792 2.5q-.708 0-1.188-.479-.479-.479-.479-1.187h3.334q0 .708-.479 1.187-.48.479-1.188.479Z"
+								/></svg
+							>
+							Perubahan Nomor Handphone
+						</div>
+					</div>
+					<div class=" flex">
+						<p class="pl-1">
+							Nomor Handphone (+62)081225365200 telah berhasil ditambahkan ke akun serbaserbi.id
+							kamu. Jika tidak merasa perubahan ini, segera hubungi xxxxxxxxx
+						</p>
+					</div>
+					<div class="flex pt-2">
+						<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"
+							><path
+								class="fill-on-surface-variant-light dark:fill-on-surface-variant-dark transition-colors duration-1000"
+								d="M12.062 13.312q.25.25.605.25.354 0 .625-.27.27-.271.27-.636 0-.364-.27-.614l-2.417-2.396V6.729q0-.354-.25-.604T10 5.875q-.354 0-.615.26-.26.261-.26.615v3.229q0 .167.063.323.062.156.187.281ZM10 18.333q-1.729 0-3.25-.656t-2.646-1.781q-1.125-1.125-1.781-2.646-.656-1.521-.656-3.25t.656-3.25q.656-1.521 1.781-2.646T6.75 2.323q1.521-.656 3.25-.656t3.25.656q1.521.656 2.646 1.781t1.781 2.646q.656 1.521.656 3.25t-.656 3.25q-.656 1.521-1.781 2.646t-2.646 1.781q-1.521.656-3.25.656ZM10 10Zm0 6.583q2.729 0 4.656-1.927 1.927-1.927 1.927-4.656 0-2.729-1.927-4.656Q12.729 3.417 10 3.417q-2.729 0-4.656 1.927Q3.417 7.271 3.417 10q0 2.729 1.927 4.656Q7.271 16.583 10 16.583Z"
+							/></svg
+						>
+						<div
+							class="pl-1 text-on-surface-variant-light dark:text-on-surface-variant-dark transition-colors duration-1000"
+						>
+							31-01-2023 16.57
+						</div>
+					</div>
+				</form>
+			</div>
+
+			<div class="flex">
+				<div class="flex-none my-auto pl-4">
+					<img
+						src="https://fastly.picsum.photos/id/1071/50/50.jpg?hmac=8UEBgUSeqRWF6cy_Hgn3imi8Sjt-AZFtGtnInAiMrKA"
+						alt="gambar promo"
+						loading="lazy"
+						class="inset-0 rounded-xl "
+					/>
+				</div>
+				<form class="flex-auto p-3">
+					<div class="flex flex-wrap">
+						<div class="flex flex-nowrap">
+							<svg
+								class="fill-tertiary-light dark:fill-tertiary-dark transition-colors duration-1000"
+								xmlns="http://www.w3.org/2000/svg"
+								height="20"
+								width="20"
+								><path
+									d="M4.208 15.833q-.354 0-.614-.26-.261-.261-.261-.615t.261-.614q.26-.261.614-.261h.771V8.354q0-1.708 1.042-3.052 1.041-1.344 2.687-1.76v-.584q0-.541.375-.916T10 1.667q.542 0 .917.375t.375.916v.584q1.646.416 2.677 1.76Q15 6.646 15 8.354v5.729h.792q.354 0 .614.261.261.26.261.614t-.261.615q-.26.26-.614.26Zm5.792 2.5q-.708 0-1.188-.479-.479-.479-.479-1.187h3.334q0 .708-.479 1.187-.48.479-1.188.479Z"
+								/></svg
+							>
+							Informasi Live Promo
+						</div>
+					</div>
+					<div class=" flex">
+						<p class="pl-1">
+							Selamat Siang Kak, Rabu 29-02-2013 akan ada live. Banyak promo tentunya
+							yachh...PANTENGIN Kuyyyyy
+						</p>
+					</div>
+					<div class="flex pt-2">
+						<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20"
+							><path
+								class="fill-on-surface-variant-light dark:fill-on-surface-variant-dark transition-colors duration-1000"
+								d="M12.062 13.312q.25.25.605.25.354 0 .625-.27.27-.271.27-.636 0-.364-.27-.614l-2.417-2.396V6.729q0-.354-.25-.604T10 5.875q-.354 0-.615.26-.26.261-.26.615v3.229q0 .167.063.323.062.156.187.281ZM10 18.333q-1.729 0-3.25-.656t-2.646-1.781q-1.125-1.125-1.781-2.646-.656-1.521-.656-3.25t.656-3.25q.656-1.521 1.781-2.646T6.75 2.323q1.521-.656 3.25-.656t3.25.656q1.521.656 2.646 1.781t1.781 2.646q.656 1.521.656 3.25t-.656 3.25q-.656 1.521-1.781 2.646t-2.646 1.781q-1.521.656-3.25.656ZM10 10Zm0 6.583q2.729 0 4.656-1.927 1.927-1.927 1.927-4.656 0-2.729-1.927-4.656Q12.729 3.417 10 3.417q-2.729 0-4.656 1.927Q3.417 7.271 3.417 10q0 2.729 1.927 4.656Q7.271 16.583 10 16.583Z"
+							/></svg
+						>
+						<div
+							class="pl-1 text-on-surface-variant-light dark:text-on-surface-variant-dark transition-colors duration-1000"
+						>
+							31-01-2023 16.57
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</PageTransition>
